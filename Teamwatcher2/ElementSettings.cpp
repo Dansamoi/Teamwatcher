ed02cba17f72b259896aa9372490e07cc7b1cd18
Dansamoi <dankot2004@gmail.com>
2023-10-24 22:40:58 +0300

    implemented multiple attributes lists

--- Teamwatcher2/ElementSettings.cpp
+++ Teamwatcher2/ElementSettings.cpp
@@ -2,18 +2,30 @@
 
 namespace GUI {
 
-	bool ElementSettings::is_set(const ElementAttributes& attribute) const
-	{
+	template<typename AttributesEnum>
+	bool Attributes<AttributesEnum>::is_set(const AttributesEnum& attribute) const {
 		return m_flags[static_cast<size_t>(attribute)];
 	}
 
-	ElementSettings::operator DWORD() const
-	{
-		DWORD attr = 0;
-		#define ADD_ATTR(name, value) attr |= is_set(ElementAttributes::name) ? value : attr;
-		ELEMENT_ATTRIBUTES(ADD_ATTR)
-		#undef	ADD_ATTR
-			return attr;
+	template<typename AttributesEnum>
+	Attributes<AttributesEnum>::operator DWORD() const {
+		static_assert(false, "Enum not of attributes");
 	}
+	
+#define ADD_ATTR(name, value)	attr |= is_set(EnumName::name) ? value : 0;
+#define GENERATE_CAST_FUNC(attr_list, enum_name)							\
+	enum_name ## Settings::operator DWORD() const {							\
+		DWORD attr = 0;														\
+		{																	\
+			using EnumName = enum_name ## Attributes;						\
+			attr_list(ADD_ATTR)												\
+		}																	\
+		return attr;														\
+	}
+
+	ATTRIBUTE_LISTS(GENERATE_CAST_FUNC)
+
+#undef	ADD_ATTR
+#undef GENERATE_COUNT_FUNCS
 
 }
--- Teamwatcher2/ElementSettings.hpp
+++ Teamwatcher2/ElementSettings.hpp
@@ -4,48 +4,89 @@
 
 namespace GUI {
 
+// Attributes to any element
 #define ELEMENT_ATTRIBUTES(X)						\
-	X(WITH_BORDER,			WS_BORDER		)		\
-	X(WITH_CAPTION,			WS_CAPTION		)		\
-	X(IS_CHILD,				WS_CHILD		)		\
 	X(CLIP_CHILDREN,		WS_CLIPCHILDREN	)		\
 	X(CLIP_SIBLINGS,		WS_CLIPSIBLINGS	)		\
 	X(DISABLED,				WS_DISABLED		)		\
+	X(VISIBLE,				WS_VISIBLE		)		\
+
+// Attributes for window
+#define WINDOW_ATTRIBUTES(X)						\
+	ELEMENT_ATTRIBUTES(X)							\
+	X(WITH_BORDER,			WS_BORDER		)		\
 	X(DIALOG_FRAME,			WS_DLGFRAME		)		\
+	X(IS_OVERLAPPED,		WS_OVERLAPPED	)		\
+	X(HAS_SYSMENU,			WS_SYSMENU		)		\
+	X(WITH_CAPTION,			WS_CAPTION		)		\
 	X(HORIZONTAL_SCROLL,	WS_HSCROLL		)		\
 	X(MAXIMIZED,			WS_MAXIMIZE		)		\
-	X(CAN_MAXIMIZE,			WS_MAXIMIZEBOX	)		\
+	X(MAXIMIZABLE,			WS_MAXIMIZEBOX	)		\
 	X(MINIMIZED,			WS_MINIMIZE		)		\
-	X(CAN_MINIMIZE,			WS_MINIMIZEBOX	)		\
-	X(IS_OVERLAPPED,		WS_OVERLAPPED	)		\
-	X(IS_POPUP,				WS_POPUP		)		\
-	X(RESIZABLE,			WS_SIZEBOX		)		\
-	X(HAS_SYSMENU,			WS_SYSMENU		)		\
-	X(TAB_FOCUS,			WS_TABSTOP		)		\
-	X(VISIBLE,				WS_VISIBLE		)		\
+	X(MINIMIZABLE,			WS_MINIMIZEBOX	)		\
 	X(VERTICAL_SCROLL,		WS_VSCROLL		)		\
+	X(RESIZABLE,			WS_SIZEBOX		)		\
+	//X(POPUP,				WS_POPUP		)		\
 
-	constexpr static size_t ElementAttributesCount() {
-		size_t count = 0;
-		#define INC_COUNT(name, value) ++count;
-		ELEMENT_ATTRIBUTES(INC_COUNT)
-		#undef INC_COUNT
-		return count;
-	}
+// Attributes for UI elements
+#define UI_ATTRIBUTES(X)							\
+	ELEMENT_ATTRIBUTES(X)							\
+	X(TAB_FOCUS,			WS_TABSTOP		)		\
 
-	enum class ElementAttributes : uint8_t {
-		#define DEFINE_ELEMENT(name, value) name,
-		ELEMENT_ATTRIBUTES(DEFINE_ELEMENT)
-		#undef DEFINE_ELEMENT
-	};
+
+#define ATTRIBUTE_LISTS(X)							\
+	X(WINDOW_ATTRIBUTES,	Window)					\
+	X(UI_ATTRIBUTES,		Element	)				\
+	// X(ELEMENT_ATTRIBUTES, General)
+
 
 	/*
-	 * Class for Setting for class
+	 * Generate enums
 	 */
-	class ElementSettings {
+#define DEFINE_ELEMENT(name, value) name,
+#define GENERATE_ENUM(attr_list, enum_name)					\
+	enum class enum_name ## Attributes : uint8_t {			\
+		attr_list(DEFINE_ELEMENT)							\
+	};
+
+	ATTRIBUTE_LISTS(GENERATE_ENUM)
+	
+#undef DEFINE_ELEMENT
+#undef GENERATE_ENUM
+
+	/*
+	 * Counts for lists
+	 */
+	template<typename Enum>
+	constexpr static size_t AttributesCount() {
+		static_assert(false, "Enum not of attributes");
+	}
+
+#define INC_COUNT(name, value) ++count;
+#define GENERATE_COUNT_FUNC(attr_list, enum_name)							\
+	template<>																\
+	constexpr static size_t AttributesCount<enum_name ## Attributes>() {	\
+		size_t count = 0;													\
+		attr_list(INC_COUNT)												\
+		return count;														\
+	}
+
+	ATTRIBUTE_LISTS(GENERATE_COUNT_FUNC)
+
+#undef INC_COUNT
+#undef GENERATE_COUNT_FUNCS
+
+	/*
+	 * Class for Setting for element
+	 */
+	template<typename AttributesEnum>
+	class Attributes {
 	public:
-		template<typename... Attributes>
-		constexpr ElementSettings(Attributes... attributes);
+		template<typename... Attr>
+		constexpr Attributes(Attr... attributes) {
+			m_flags.reset();
+			set_attributes(attributes...);
+		}
 
 		constexpr void set_attributes() {}
 		constexpr void unset_attributes() {}
@@ -53,19 +94,31 @@ namespace GUI {
 		/*
 		 * Sets attributes in settings
 		 */
-		template<typename... Attributes>
-		constexpr void set_attributes(ElementAttributes attribute, Attributes... attributes);
+		template<typename... Attr>
+		constexpr void set_attributes(AttributesEnum attribute, Attr... attributes ) {
+			static_assert(std::is_same<AttributesEnum, typename std::remove_reference<decltype(attribute)>::type>::value,
+				"All arguments must be from the enum");
+
+			m_flags[static_cast<size_t>(attribute)] = true;
+			set_attributes(attributes...);
+		}
 
 		/*
 		 * Unsets attributes in settings
 		 */
-		template<typename... Attributes>
-		constexpr void unset_attributes(ElementAttributes attribute, Attributes... attributes);
+		template<typename... Attr>
+		constexpr void unset_attributes(AttributesEnum attribute, Attr... attributes) {
+			static_assert(std::is_same<AttributesEnum, typename std::remove_reference<decltype(attribute)>::type>::value,
+				"All arguments must be from the enum");
+
+			m_flags[static_cast<size_t>(attribute)] = false;
+			unset_attributes(attributes...);
+		}
 
 		/*
 		 * Checks if attribute is set
 		 */
-		bool is_set(const ElementAttributes& attribute) const;
+		bool is_set(const AttributesEnum& attribute) const;
 
 		/*
 		 * Return value for winapi 
@@ -73,34 +126,10 @@ namespace GUI {
 		operator DWORD() const;
 
 	private:
-		std::bitset<ElementAttributesCount()> m_flags;
+		std::bitset<AttributesCount<AttributesEnum>()> m_flags;
 	};
 
-	template<typename ...Attributes>
-	constexpr inline ElementSettings::ElementSettings(Attributes... attributes)
-	{
-		m_flags.reset();
-		set_attributes(attributes...);
-	}
-
-	template<typename ...Attributes>
-	constexpr inline void ElementSettings::set_attributes(ElementAttributes attribute, Attributes ...attributes)
-	{
-		static_assert(std::is_same<ElementAttributes, typename std::remove_reference<decltype(attribute)>::type>::value,
-			"All arguments must be of type ElementAttributes");
-
-		m_flags[static_cast<size_t>(attribute)] = true;
-		set_attributes(attributes...);
-	}
-
-	template<typename ...Attributes>
-	inline constexpr void ElementSettings::unset_attributes(ElementAttributes attribute, Attributes ...attributes)
-	{
-		static_assert(std::is_same<ElementAttributes, typename std::remove_reference<decltype(attribute)>::type>::value,
-			"All arguments must be of type ElementAttributes");
-
-		m_flags[static_cast<size_t>(attribute)] = false;
-		unset_attributes(attributes...);
-	}
-
+#define DEFINE_TYPE(attr_list, enum_name)	using enum_name ## Settings = Attributes<enum_name ## Attributes>;
+	ATTRIBUTE_LISTS(DEFINE_TYPE)
+#undef DEFINE_TYPE
 }
